2fd5a6c125f00e9d2ae2df6ac30ad500fd602b99
Si Li <lisztlee@live.com>
2021-04-25 12:35:54 -0700

    deploy

---
+++ target/classes/public/precache-manifest.4b758aaf873be9052f98c9890fb152a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8fd2c4ea7ae8ea6c22254e21e5cac90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03d0444a1f49206a751e",
+    "url": "/static/css/main.b43c1d2e.chunk.css"
+  },
+  {
+    "revision": "65434565335485e5d9d8",
+    "url": "/static/js/2.10728dbd.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.10728dbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d0444a1f49206a751e",
+    "url": "/static/js/main.9d9feb80.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  }
+]);
